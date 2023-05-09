548c91f43850e05786557bbec2157af5af5bf873
Tommaso <dptoolkit@gmail.com>
2023-05-09 23:53:57 +0200

    eee

---
+++ index.js
@@ -0,0 +1,29 @@
+async function loadJSON(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+
+function createItemHTML(item) {
+    const authors = item.author.map(author => `${author.family}, ${author.given}`).join('; ');
+    const issued = item.issued['date-parts'][0].join('-');
+    const accessed = item.accessed['date-parts'][0].join('-');
+    let html = `
+    <div class="item">
+      <div class="title">${item.title}</div>
+      <div class="authors">${authors}</div>
+      <div class="publisher">${item.publisher}, ${item['publisher-place']}, ${issued}</div>
+      <div class="accessed">Accessed ${accessed}</div>
+      <div class="url"><a href="${item.URL}">${item.URL}</a></div>
+    </div>
+  `;
+    return html;
+}
+
+async function loadCatalog(url) {
+    const catalog = await loadJSON(url);
+    const itemsHTML = catalog.map(createItemHTML).join('\n');
+    document.getElementById('catalog').innerHTML = itemsHTML;
+}
+
+loadCatalog('catalog.json');
